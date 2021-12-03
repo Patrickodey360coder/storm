139e12f3a0d5e1e7493e0bc30b311ce198b0f0b9
Patrickodey360coder <patrickodey419@gmail.com>
2021-12-03 14:20:25 +0100

    coming soon page

--- resources/js/components/ComingSoon.js
+++ resources/js/components/ComingSoon.js
@@ -1,4 +1,6 @@
 import React from 'react'
+// import DateCountdown from 'react-date-countdown-timer';
+
 
 function ComingSoon() {
   return (
@@ -7,6 +9,7 @@ function ComingSoon() {
         <p>Akshiptika Blog</p>
         <h1>COMING SOON</h1>
         <hr />
+        
       </div>
     </div>
   )
