f3c47d86dff19d19145b95802af3591bb2ad33e1
patrick <patrickodey419@gmail.com>
2021-12-04 02:53:26 +0100

    added maintenance page

---
+++ resources/js/components/Maintenance.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Maintenance() {
+  return (
+    <div id="maintenance">
+      <div className="text-center">
+        <p>Akshiptika Blog</p>
+        <h1>THIS PAGE IS UNDER MAINTENANCE</h1>
+        <hr />
+        
+      </div>
+    </div>
+  )
+}
+
+export default Maintenance
